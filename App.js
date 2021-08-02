1ca19bd50739fc22c423e3d227717e51227877bf
Nikhil <nikhilkumarnk2209@gmail.com>
2021-08-02 12:58:51 +0530

    First AND final

---
+++ App.js
@@ -0,0 +1,67 @@
+import React,{useState} from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  StatusBar,
+} from "react-native";
+
+const App = () =>{
+
+  const [randomColor, setRandomColor] = useState("rgb(32, 0, 126)");
+  const changeBG = () =>{
+    let color = 'rgb('+
+      Math.floor(Math.random() * 256)+
+      ','+
+      Math.floor(Math.random() * 256)+
+      ','+
+      Math.floor(Math.random() * 256)+
+      ')';
+
+      setRandomColor(color);
+  }
+  const changeBlack = () => {
+    let colors = "#000000";
+     
+
+    setRandomColor(colors);
+  }
+
+  return(
+    <>
+    <StatusBar backgroundColor={randomColor}></StatusBar>
+      <View style={[style.container, { backgroundColor: randomColor}]}>
+        <TouchableOpacity onPress={changeBG}>
+    <Text style={style.text}>Nikhil Kumar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={changeBlack}>
+          <Text style={style.text}>Nikhil Kumar</Text>
+        </TouchableOpacity>
+    </View>
+    </>
+  );
+};
+
+
+export default App;
+
+const style = StyleSheet.create({
+  container:{
+    flex: 1,
+    alignItems: "center",
+    justifyContent:"center",
+    
+  },
+  text:{
+      fontSize:30,
+      backgroundColor:"#BB2CD9",
+      paddingVertical:10,
+      paddingHorizontal:40,
+      color:"#FFFFFF",
+      borderRadius:15,
+      textTransform:"uppercase",
+      marginBottom: 50,
+  }
+});
